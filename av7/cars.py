afccc440e4056f94d52a92a7708a0eca13e6ec66
Toshevska Martina <martina.tosevska.95@gmail.com>
2022-04-18 14:49:46 +0200

    New scripts for exercise 7 examples

--- av7/cars.py
+++ av7/cars.py
@@ -1,35 +1,54 @@
 import csv
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.naive_bayes import CategoricalNB
-import math
 
-if __name__ == '__main__':
-    with open("car.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+
+def read_file(file_name):
+    with open(file_name) as doc:
+        csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
 
-        encoder = OrdinalEncoder()
-        encoder.fit([dataset[i][:-1] for i in range(0, len(dataset))])
+    return dataset
 
-        train_set = dataset[0:math.ceil(0.7 * len(dataset))]
-        test_set = dataset[math.ceil(0.7 * len(dataset)):]
 
-        X = [train_set[i][:-1] for i in range(0, len(train_set))]
-        X = encoder.transform(X)
-        Y = [train_set[i][-1] for i in range(0, len(train_set))]
+if __name__ == '__main__':
+    dataset = read_file('car.csv')
 
-        clf = CategoricalNB()
-        clf.fit(X, Y)
+    encoder = OrdinalEncoder()
+    encoder.fit([row[:-1] for row in dataset])
 
-        test_set_x = encoder.transform([test_set[i][:-1] for i in range(0, len(test_set))])
-        accuracy = 0
-        for i in range(0,len(test_set)):
-            predict = clf.predict([test_set_x[i]])
-            if predict[0] == test_set[i][-1]:
-                accuracy += 1
+    train_set = dataset[:int(0.7 * len(dataset))]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
+    train_x = encoder.transform(train_x)
 
-        print(accuracy / len(test_set))
+    test_set = dataset[int(0.7 * len(dataset)):]
+    test_x = [row[:-1] for row in test_set]
+    test_y = [row[-1] for row in test_set]
+    test_x = encoder.transform(test_x)
 
-        entry = [el for el in input().split(' ')]
-        entry = encoder.transform([entry])
-        print(clf.predict(entry))
+    classifier = CategoricalNB()
+    classifier.fit(train_x, train_y)
+
+    predicted_class = classifier.predict([test_x[5]])[0]
+    true_class = test_y[5]
+
+    accuracy = 0
+
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
+
+    accuracy = accuracy / len(test_set)
+
+    print(f'Accuracy: {accuracy}')
+
+    entry = [el for el in input().split(' ')]
+
+    encoded_entry = encoder.transform([entry])
+
+    predicted_class = classifier.predict(encoded_entry)[0]
+
+    print(predicted_class)
--- av7/medical_data.py
+++ av7/medical_data.py
@@ -1,31 +1,50 @@
 import csv
-import math
 from sklearn.naive_bayes import GaussianNB
 
-if __name__ == '__main__':
-    with open("medical_data.csv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
+
+def read_file(file_name):
+    with open(file_name) as doc:
+        csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
-        dataset = [[int(dataset[i][j]) for j in range(0, len(dataset[i]))] for i in range(0, len(dataset))]
 
-        train_set = dataset[0:math.ceil(0.8 * len(dataset))]
-        test_set = dataset[math.ceil(0.8 * len(dataset)):]
+    dataset_v2 = []
+    for row in dataset:
+        row_v2 = [int(el) for el in row]
+        dataset_v2.append(row_v2)
 
-        X = [train_set[i][:-1] for i in range(0, len(train_set))]
-        Y = [train_set[i][-1] for i in range(0, len(train_set))]
+    return dataset_v2
 
-        clf = GaussianNB()
-        clf.fit(X, Y)
 
-        # entry = [int(element) for element in input().split(' ')]
-        #
-        # print(clf.predict_proba([entry]))
+if __name__ == '__main__':
+    dataset = read_file('medical_data.csv')
 
-        accuracy = 0
+    train_set = dataset[:int(0.7 * len(dataset))]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
 
-        for row in test_set:
-            prediction = clf.predict([row[:-1]])
-            if prediction[0] == row[-1]:
-                accuracy += 1
+    test_set = dataset[int(0.7 * len(dataset)):]
+    test_x = [row[:-1] for row in test_set]
+    test_y = [row[-1] for row in test_set]
 
-        print(accuracy/len(test_set))
+    classifier = GaussianNB()
+    classifier.fit(train_x, train_y)
+
+    accuracy = 0
+
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
+
+    accuracy = accuracy / len(test_set)
+
+    print(f'Accuracy: {accuracy}')
+
+    entry = [int(el) for el in input().split(' ')]
+
+    predicted_class = classifier.predict([entry])[0]
+
+    print(predicted_class)
+
+    print(classifier.predict_proba([entry]))
