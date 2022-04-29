3f6fd5df847e1ad8dbad1d84f2fed2a5d32f0953
Toshevska Martina <martina.tosevska.95@gmail.com>
2022-04-29 14:35:39 +0200

    New scripts for exercise 8 examples

--- av8/car_acceptability_decision_tree.py
+++ av8/car_acceptability_decision_tree.py
@@ -2,94 +2,109 @@ import csv
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeClassifier
 
-if __name__ == '__main__':
-    with open('car.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+def read_file(file_name):
+    with open(file_name) as doc:
+        csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
+
+    return dataset
+
+if __name__ == '__main__':
+    dataset = read_file('car.csv')
 
     encoder = OrdinalEncoder()
     encoder.fit([row[:-1] for row in dataset])
 
     train_set = dataset[:int(0.7 * len(dataset))]
-    train_x = [t[:-1] for t in train_set]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
     train_x = encoder.transform(train_x)
-    train_y = [t[-1] for t in train_set]
 
     test_set = dataset[int(0.7 * len(dataset)):]
-    test_x = [t[:-1] for t in test_set]
+    test_x = [row[:-1] for row in test_set]
+    test_y = [row[-1] for row in test_set]
     test_x = encoder.transform(test_x)
-    test_y = [t[-1] for t in test_set]
 
-    classifier = DecisionTreeClassifier(criterion='entropy')
+    # classifier = DecisionTreeClassifier(criterion='entropy', max_depth=5, max_leaf_nodes=20, random_state=0)
+    classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
     classifier.fit(train_x, train_y)
 
     print(f'Depth: {classifier.get_depth()}')
     print(f'Number of leaves: {classifier.get_n_leaves()}')
 
-    correct_samples = 0
-    for x, y in zip(test_x, test_y):
-        y_predicted = classifier.predict([x])[0]
-        if y == y_predicted:
-            correct_samples += 1
+    accuracy = 0
 
-    accuracy = correct_samples / len(test_set)
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
+
+    accuracy = accuracy / len(test_set)
+
     print(f'Accuracy: {accuracy}')
 
-    feature_importances = list(classifier.feature_importances_)
-    print(f'Feature importances: {feature_importances}')
+    features_importances = list(classifier.feature_importances_)
+    print(f'Feature importances: {features_importances}')
 
-    most_important_feature = feature_importances.index(max(feature_importances))
+    most_important_feature = features_importances.index(max(features_importances))
     print(f'Most important feature: {most_important_feature}')
 
-    least_important_feature = feature_importances.index(min(feature_importances))
+    least_important_feature = features_importances.index(min(features_importances))
     print(f'Least important feature: {least_important_feature}')
 
     train_x_2 = list()
     for t in train_x:
-        sample = [t[i] for i in range(len(t)) if i != most_important_feature]
-        train_x_2.append(sample)
+        row = [t[i] for i in range(len(t)) if i != most_important_feature]
+        train_x_2.append(row)
 
     test_x_2 = list()
     for t in test_x:
-        sample = [t[i] for i in range(len(t)) if i != most_important_feature]
-        test_x_2.append(sample)
+        row = [t[i] for i in range(len(t)) if i != most_important_feature]
+        test_x_2.append(row)
 
     train_x_3 = list()
     for t in train_x:
-        sample = [t[i] for i in range(len(t)) if i != least_important_feature]
-        train_x_3.append(sample)
+        row = [t[i] for i in range(len(t)) if i != least_important_feature]
+        train_x_3.append(row)
 
     test_x_3 = list()
     for t in test_x:
-        sample = [t[i] for i in range(len(t)) if i != least_important_feature]
-        test_x_3.append(sample)
+        row = [t[i] for i in range(len(t)) if i != least_important_feature]
+        test_x_3.append(row)
 
-    classifier2 = DecisionTreeClassifier(criterion='entropy')
-    classifier3 = DecisionTreeClassifier(criterion='entropy')
-
+    classifier2 = DecisionTreeClassifier(criterion='entropy', random_state=0)
     classifier2.fit(train_x_2, train_y)
+
+    classifier3 = DecisionTreeClassifier(criterion='entropy', random_state=0)
     classifier3.fit(train_x_3, train_y)
 
-    print(f'Depth (removed most important feature): {classifier2.get_depth()}')
-    print(f'Number of leaves (removed most important feature): {classifier2.get_n_leaves()}')
+    print(f'Depth (without most important feature): {classifier2.get_depth()}')
+    print(f'Number of leaves (without most important feature): {classifier2.get_n_leaves()}')
 
-    print(f'Depth (removed least important feature): {classifier3.get_depth()}')
-    print(f'Number of leaves (removed least important feature): {classifier3.get_n_leaves()}')
+    print(f'Depth (without least important feature): {classifier3.get_depth()}')
+    print(f'Number of leaves (without least important feature): {classifier3.get_n_leaves()}')
 
-    correct_samples2 = 0
-    for x, y in zip(test_x_2, test_y):
-        y_predicted = classifier2.predict([x])[0]
-        if y == y_predicted:
-            correct_samples2 += 1
+    accuracy2 = 0
 
-    correct_samples3 = 0
-    for x, y in zip(test_x_3, test_y):
-        y_predicted = classifier3.predict([x])[0]
-        if y == y_predicted:
-            correct_samples3 += 1
+    for i in range(len(test_set)):
+        predicted_class = classifier2.predict([test_x_2[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy2 += 1
 
-    accuracy2 = correct_samples2 / len(test_set)
-    accuracy3 = correct_samples3 / len(test_set)
+    accuracy2 = accuracy2 / len(test_set)
 
-    print(f'Accuracy (removed most important feature): {accuracy2}')
-    print(f'Accuracy (removed least important feature): {accuracy3}')
+    print(f'Accuracy (without most important feature): {accuracy2}')
+
+    accuracy3 = 0
+
+    for i in range(len(test_set)):
+        predicted_class = classifier3.predict([test_x_3[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy3 += 1
+
+    accuracy3 = accuracy3 / len(test_set)
+
+    print(f'Accuracy (without least important feature): {accuracy3}')
--- av8/car_acceptability_random_forest.py
+++ av8/car_acceptability_random_forest.py
@@ -2,40 +2,55 @@ import csv
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import RandomForestClassifier
 
-if __name__ == '__main__':
-    with open('car.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+def read_file(file_name):
+    with open(file_name) as doc:
+        csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
+
+    return dataset
+
+if __name__ == '__main__':
+    dataset = read_file('car.csv')
 
     encoder = OrdinalEncoder()
     encoder.fit([row[:-1] for row in dataset])
 
     train_set = dataset[:int(0.7 * len(dataset))]
-    train_x = [t[:-1] for t in train_set]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
     train_x = encoder.transform(train_x)
-    train_y = [t[-1] for t in train_set]
 
     test_set = dataset[int(0.7 * len(dataset)):]
-    test_x = [t[:-1] for t in test_set]
+    test_x = [row[:-1] for row in test_set]
+    test_y = [row[-1] for row in test_set]
     test_x = encoder.transform(test_x)
-    test_y = [t[-1] for t in test_set]
 
-    classifier = RandomForestClassifier(n_estimators=150, criterion='entropy')
+    classifier = RandomForestClassifier(n_estimators=150, criterion='entropy', random_state=0)
     classifier.fit(train_x, train_y)
 
-    correct_samples = 0
-    for x, y in zip(test_x, test_y):
-        y_predicted = classifier.predict([x])[0]
-        if y_predicted == y:
-            correct_samples += 1
+    accuracy = 0
 
-    print(f'Accuracy: {correct_samples / len(test_set)}')
+    for i in range(len(test_set)):
+        predicted_class = classifier.predict([test_x[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy += 1
 
-    feature_importances = list(classifier.feature_importances_)
-    most_important_feature = feature_importances.index(max(feature_importances))
-    least_important_feature = feature_importances.index(min(feature_importances))
+    accuracy = accuracy / len(test_set)
 
+    print(f'Accuracy: {accuracy}')
+
+    features_importances = list(classifier.feature_importances_)
+    print(f'Feature importances: {features_importances}')
+
+    most_important_feature = features_importances.index(max(features_importances))
     print(f'Most important feature: {most_important_feature}')
+
+    least_important_feature = features_importances.index(min(features_importances))
     print(f'Least important feature: {least_important_feature}')
+
+
+
+
 
 
